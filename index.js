ac1b01d4384f7d46f1fa256632f26b6fa2e2789d
andreyudi10 <andrianus.yudi@gmail.com>
2021-05-17 20:07:02 +0700

    use node index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require('express')
 
-const app = express()
+app = express()
 
+port = process.env.port || 3000;
+
+app.get('/', ( req, res ) => {
+    res.send('hello world')
+})
+
+app.listen( port, () => {
+    console.log(`server start at ${port}`)
+})
